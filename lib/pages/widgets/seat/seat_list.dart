76fe5ca06123ea6d256b7161e986028ffda968a9
hippieperm <leess0133@gmail.com>
2025-04-01 23:32:44 +0900

    ✨ TrainSelectBox, TrainList, SeatList 위젯 추가: 기차역 선택 및 목록 표시 UI 구현

---
+++ lib/pages/widgets/seat/seat_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeatList extends StatelessWidget {
+  const SeatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
