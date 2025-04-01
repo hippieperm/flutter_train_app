1f11b67dc3710ea36eda97cd26afe76a2d9e67d9
hippieperm <leess0133@gmail.com>
2025-04-01 23:12:58 +0900

    ✨ StationListPage에서 기차역 목록 표시 방식을 TrainList 위젯으로 변경하여 코드 간소화

--- lib/pages/station_list_page.dart
+++ lib/pages/station_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:a/data/data.dart';
+import 'package:a/pages/widgets/train_list.dart';
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget {
@@ -16,33 +17,7 @@ class StationListPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView.builder(
-          itemCount: trainStations.length,
-          itemBuilder: (context, index) => Container(
-            height: 50,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.grey[300]!,
-                  width: 1,
-                ),
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '${trainStations[index]}',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        child: TrainList(),
       ),
     );
   }
