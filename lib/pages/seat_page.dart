67646da0e01a7ce9efd4dfe6029845cc6d785db1
hippieperm <leess0133@gmail.com>
2025-04-01 23:33:05 +0900

    ✨ SeatPage의 UI 개선: AppBar 추가 및 출발역/도착역 헤더 및 좌석 전설 구성 요소 구현

--- lib/pages/seat_page.dart
+++ lib/pages/seat_page.dart
@@ -5,6 +5,75 @@ class SeatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('SeatPage');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('좌석 선택'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Column(
+        children: [
+          buildStationHeader(),
+          SizedBox(height: 20),
+          buildSeatLegend(),
+        ],
+      ),
+    );
   }
+
+  Row buildStationHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          '출발역',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.purple,
+            fontSize: 30,
+          ),
+        ),
+        Icon(Icons.arrow_circle_right_outlined),
+        Text(
+          '도착역',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.purple,
+            fontSize: 30,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Row buildSeatLegend() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        width: 24,
+        height: 24,
+        decoration: BoxDecoration(
+          color: Colors.purple,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      SizedBox(width: 4),
+      Text('선택됨'),
+      SizedBox(width: 20),
+      Container(
+        width: 24,
+        height: 24,
+        decoration: BoxDecoration(
+          color: Colors.grey[300]!,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      SizedBox(width: 4),
+      Text('선택됨'),
+    ],
+  );
 }
