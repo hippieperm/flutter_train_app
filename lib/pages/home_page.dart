2592e5c2ef2e619518a0fd44a02c0cb76561b070
hippieperm <leess0133@gmail.com>
2025-04-01 20:25:34 +0900

    ✨ 홈 페이지의 선택 박스 추가 및 불필요한 레이아웃 코드 제거

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:a/pages/widgets/select_box.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -21,55 +22,7 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        '출발역',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        '선택',
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: 2,
-                    height: 50,
-                    color: Colors.grey[400],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '출발역',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        '선택',
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              child: SelectBox(),
             )
           ],
         ),
@@ -77,3 +30,6 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+
