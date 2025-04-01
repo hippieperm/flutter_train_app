210b3e2765375e229906d69a0b1b726d0a9019da
hippieperm <leess0133@gmail.com>
2025-04-01 20:38:24 +0900

    ✨ StationListPage 위젯에 const 키워드 추가

--- lib/pages/station_list_page.dart
+++ lib/pages/station_list_page.dart
@@ -5,6 +5,6 @@ class StationListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('StationListPage');
+    return const Text('StationListPage');
   }
 }
